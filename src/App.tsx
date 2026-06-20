1ca6ee2c2efc83b90e2dd07dabdee989f334691a
eliaspc2 <eliaspc2@users.noreply.github.com>
2026-06-20 11:41:42 +0100

    Reframe EFA completion and internship timing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { validateConfig, type ClassPresetGroup, type Config } from './validator'
 
 const STORAGE_KEY = 'json-site-lang';
 const DEFAULT_STRINGS_FILE = 'data/uk-en.json';
-const APP_BUILD_VERSION = '2026-06-18.19';
+const APP_BUILD_VERSION = '2026-06-20.1';
 const DATA_CACHE_KEY = APP_BUILD_VERSION;
 
 const FOOTER_DOCUMENT_LINKS = [
@@ -449,7 +449,7 @@ export default function App() {
         classKeys: classKeysFile
           ? withCacheVersion(toPublicUrlIfRelative(classKeysFile) || toPublicUrl(classKeysFile))
           : 'inline',
-        serviceWorker: 'json-site-v34'
+        serviceWorker: 'json-site-v35'
       });
       setClassPresetTree(classTree || {});
       setClassPresetMap(flattenClassPresets(classTree));
